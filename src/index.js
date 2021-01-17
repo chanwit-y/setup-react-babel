05fbcb8c04f94c07f8a7c5f7c1e0168041251dd2
chanwit-y <chanwit.yimneam@hotmail.com>
2021-01-17 20:24:01 +0700

    delete all

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-
-const title = 'Test'
-
-ReactDom.render(<div>{title}</div>, document.getElementById("root"));
-
-module.hot.acceph();
